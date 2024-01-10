a8d5409dddf88feb3c70ddfe933dd8e4ae1d6251
Vanessa Mae <vanesssamae23@gmail.com>
2024-01-10 22:13:43 +0700

    Initial Commit

---
+++ BallDemo/BallDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallDemoApp.swift
+//  BallDemo
+//
+//  Created by MacBook Pro on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BallDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
